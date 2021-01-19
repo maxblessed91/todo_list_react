bfd0f225b32b1d90e82591c160786f4badfba1ed
maxblessed91 <vadams3@yandex.ru>
2021-01-19 19:14:33 +0400

    add classes, event rendering and state

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,33 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './todo-list-item.css';
 
-const TodoListItem = ( { label, important = false } ) => {
+export default class TodoListItem extends Component {
 
-    const style = {
-        color: important ? 'steelblue' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
-    };
+    state = {
+        done: false
+    }; // Добавляем состояние реакт компонента
 
-    return (
-        <span className="todo-list-item">
-            <span
-                className="todo-list-item-label"
-                style={style}>
-                { label }
+    onLabelClick = () => {
+        // console.log(`Done: ${this.props.label}`);
+        this.setState({
+            done: true
+        });
+    }; // Добавление EventListener
+
+
+    // constructor() {
+    //     super();
+
+    //     this.onLabelClick = () => {
+    //         console.log(`Done: ${this.props.label}`);
+    //     };
+    // } Добавление EventListener
+
+    // onLabelClick() {
+    //     console.log(`Done: ${this.props.label}`);
+    // }
+
+
+    render() {
+
+        const { label, important = false } = this.props; // Из этого места можно получить текущие свойства
+        const { done } = this.state; // используем деструктуризацию, для того чтобы достать done из state
+
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        }
+
+        const style = {
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        };
+
+        return (
+            <span className={classNames}>
+                <span
+                    className="todo-list-item-label"
+                    style={style}
+                    onClick={ this.onLabelClick }>
+                    { label }
+                </span>
+
+                <button type='button'
+                        className='btn btn-outline-success btn-sm float-right'>
+                    <i className='fa fa-exclamation'/>
+                </button>
+
+                <button type='button'
+                        className='btn btn-outline-danger btn-sm float-right'>
+                    <i className='fa fa-trash-o'/>
+                </button>
             </span>
-
-            <button type='button'
-                    className='btn btn-outline-success btn-sm float-right'>
-                <i className='fa fa-exclamation'/>
-            </button>
-
-            <button type='button'
-                    className='btn btn-outline-danger btn-sm float-right'>
-                <i className='fa fa-trash-o'/>
-            </button>
-        </span>
-    );
-};
-
-export default TodoListItem;
+        );
+    }
+}
