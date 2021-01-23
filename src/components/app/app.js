7d2da8a662f57d9c4f3cb69f09cfcd835ac8a7ca
maxblessed91 <vadams3@yandex.ru>
2021-01-23 17:45:41 +0400

    add working custom event onDeleted

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react'; // Подключаем библиотеки
+import React, { Component } from 'react'; // Подключаем библиотеки
 
 
 import AppHeader from '../app-header';
@@ -7,26 +7,46 @@ import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-    const todoData = [
+    state = {
+        todoData: [
         { label: 'Drink Coffee', important: false, id: 1 },
         { label: 'Make Awesome App', important: true, id: 2 },
         { label: 'Have a lunch', important: false, id: 3 }
-    ];
-
-    // const value = '<script>alert("")</script>';
-    return (
-        <div className='todo-app'>
-            <AppHeader toDo={1} done={3} />
-            <div className='top-panel d-flex'>
-                <SearchPanel />
-                <ItemStatusFilter />
-            </div>
-            <TodoList todos={todoData}
-            onDeleted={ (id) => console.log('del', id) }/>
-        </div>
-    );
+    ]
 };
 
-export default App;
+deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+        const idx = todoData.findIndex((el) => el.id === id);
+
+        // const before = todoData.slice(0, idx); // берем все элементы до удаленного
+        // const after = todoData.slice(idx + 1); // берем все элементы после удаленного(slice не изменяет изначальный массив)
+
+        const newArray = [
+            ...todoData.slice(0, idx),
+            ...todoData.slice(idx + 1)
+        ]; // конструируем новый массив из всех элементов до нужного и после нужного
+
+        return {
+            todoData: newArray // наш новый массив
+        };
+    });
+};
+
+    render() {
+        return (
+            <div className='todo-app'>
+                <AppHeader toDo={1} done={3} />
+                <div className='top-panel d-flex'>
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+                <TodoList todos={this.state.todoData}
+                onDeleted={ this.deleteItem }/>
+            </div>
+        );
+    };
+    }
+    // const value = '<script>alert("")</script>';
